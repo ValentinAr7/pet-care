ab0ac2bd45bdffe5a842369c048abd235f84a612
Valentin <valio990210@gmail.com>
2023-02-24 10:18:52 +0200

    routing

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
-import * as api from '/api/user.js'
+// import * as api from './api/user.js'        //import *all items 
+// window.api = api      //assign the imported module object to the global object
 
+import {page, render } from './lib.js'
 
-window.api = api
+page('/', ())
