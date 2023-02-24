62b82e222d2b10672abffda23ea7082feab8dba3
Valentin <valio990210@gmail.com>
2023-02-24 09:46:06 +0200

    create 4 requests

--- src/api/api.js
+++ src/api/api.js
@@ -40,3 +40,8 @@ async function request(method, url, data){ //make HTTP request to the server
         throw err
     }
 }
+
+export const get = request.bind(null, 'get');           
+export const post = request.bind(null, 'post');
+export const put = request.bind(null, 'put');
+export const del = request.bind(null, 'delete');
