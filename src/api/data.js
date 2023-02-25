c379773b673fceac6191674d43489e2d2400353e
Valentin <valio990210@gmail.com>
2023-02-25 11:39:22 +0200

    details section

--- src/api/data.js
+++ src/api/data.js
@@ -3,3 +3,13 @@ import { get } from "./api.js";
 export async function getAll(){         //read info from catalog.js
     return get('/data/pets?sortBy=_createdOn%20desc&distinct=name')
 }
+
+
+export async function getById(id){
+    return get('/data/pets' + id)
+}
+
+
+export async function deleteById(id){
+    return del('/data/pets/' + id);
+}
--- src/app.js
+++ src/app.js
@@ -1,21 +1,22 @@
 // import * as api from './api/user.js'        //import *all items 
 // window.api = api      //assign the imported module object to the global object
 
-import { logout } from './api/user.js';
 import {page, render } from './lib.js'
+import { getUserData } from './util.js';
 import { showCatalog } from './views/catalog.js';
+import { showDetails } from './views/details.js';
 import { showHome } from './views/home.js';
 import { showLogin } from './views/login.js';
 import { updateNav } from './views/nav.js';
 import { showRegister } from './views/register.js';
 
 const main = document.getElementById('content')     //content needs to get to all views
-document.getElementById('logoutBtn').addEventListener('click', onLogout)
+// document.getElementById('logoutBtn').addEventListener('click', onLogout)
 
 page(decorateContext);
 page('/', showHome);
 page('/catalog', showCatalog);
-page('/catalog/:id', () => console.log('details'));
+page('/catalog/:id', showDetails);
 page('/edit/:id', () => console.log('edit'));
 page('/create', () => console.log('create'));
 page('/register', showRegister);
@@ -27,6 +28,12 @@ page.start()
 function decorateContext(ctx, next){     //every time the user navigates page.js will run this function before every view
     ctx.render = renderMain;
     ctx.updateNav = updateNav;
+
+    const user = getUserData()  //check if the IDs if the user match
+    if(user){
+        ctx.user = user
+    }
+
 
     next()          //continue in case of async func
 }
